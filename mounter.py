2422627b253f00ec10630b3b0b2ad6c8e35526dd
hackerman234 <56136248+hackerman234@users.noreply.github.com>
2019-10-23 23:39:38 -0400

    Update mounter.py

--- mounter.py
+++ mounter.py
@@ -8,28 +8,30 @@ def main():
       parser.add_argument("-r", type=str, dest="ram", help="checks ram", default=None)
       parser.add_argument("-m", type=str, dest="mount", help="mounts space", default=None)
 
-
       args = parser.parse_args()
 
       ram = ""
+      mount = ""
       ram = args.ram
+      mount = args.mount
 
       if ram:
           ram_check(ram)
+      elif mount:
+          mounter(mount)
       else:
           print("[-] Invaled options")
- 
+    
+#Report on ram stats
 def ram_check(ram):
     print("[+] Displaying ram...")
     subprocess.run(["free", "-g"])
-   #needs changed to Gigs
 
-# -m Mount the ramdisk
 def mounter(mount):
     mount_size = input("[*] Enter how much space you would like in gigabytes: ")
     print("[+] Making directory in /mnt/ramdisk")
     subprocess.run(["mkdir", "/mnt/ramdisk"])
     print("[+] Mounting " + mount_size + " to /mnt/ramdisk")
-    subprocess.run(["mount", "-t", "tmpfs", "-o", "size=<mount_size>", "tmpfs", "/mnt/ramdisk",])
+    subprocess.run(["mount", "-t", "tmpfs", "-o", "size=", "tmpfs", "/mnt/ramdisk",])
 
 main()
