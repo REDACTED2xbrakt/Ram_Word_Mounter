6274ea52fa711e4b77dac30b7e451908245f6a13
9shadesleft <46411461+9shadesleft@users.noreply.github.com>
2019-11-26 22:47:07 -0500

    Pulling it! (#2)
    * Update to mounter.py
    
    edited python, splitting one argument into two, word clarification, and added "g" to "mount_size" to make sure that it mounts the proper size (gigs not megs). Needs code review
    
    * Update mounter.py
    
    * typo

--- mounter.py
+++ mounter.py
@@ -4,7 +4,8 @@ import subprocess, argparse
 def main():
       parser = argparse.ArgumentParser()
       parser.add_argument("-ram", type=str, dest="ram", help="checks ram", default=None)
-      parser.add_argument("-mount", type=str, dest="mount", help="mounts space", default=None)
+      parser.add_argument("-mount", type=str, dest="mount", help="mounts ramdisk", default=None)
+      parser.add_argument("-word", type=str, dest="word", help="copies wordlist to ram", default=None)
 
       args = parser.parse_args()
 
@@ -15,6 +16,8 @@ def main():
           ram_check(ram)
       elif mount:
           mounter(mount)
+      elif wordlist:
+          wordlist(word)
       else:
           print("[-] Invalid option try -h for help")
     
@@ -26,11 +29,12 @@ def mounter(mount):
     mount_size = input("[*] Enter how much space you would like in gigabytes: ")
     print("[+] Making directory in /mnt/ramdisk")
     subprocess.run(["mkdir", "/mnt/ramdisk"])
-    print("[+] Mounting " + mount_size + " to /mnt/ramdisk")
-    subprocess.run(["mount", "-t", "tmpfs", "-o", "size=" + mount_size, "tmpfs", "/mnt/ramdisk",])
+    print("[+] Mounting " + mount_size + "gb ramdisk to /mnt/ramdisk")
+    subprocess.run(["mount", "-t", "tmpfs", "-o", "size=" + mount_size + "g", "tmpfs", "/mnt/ramdisk",])
     print("[+] Done!")
-    wordlist = input("[*] Enter wordlist you would like to put in the ram: ")
-    subprocess.run(["cp", wordlist])
+def wordlist(word):
+    wordlist = input("[*] Enter the directory of the wordlist you would like to put in the ram: ")
+    subprocess.run(["cp", wordlist, "/mnt/ramdisk/"])
     print("[+] Done!")
 
 
